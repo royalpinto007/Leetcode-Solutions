ef693229155e31e23a3b2bccef2bfa9b632f16c5
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-08-31 10:38:44 +0530

    Time: 20 ms (65.08%), Space: 10.9 MB (47.42%) - LeetHub

---
+++ 1-two-sum/1-two-sum.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+      
+        int low=0;
+        int high=nums.size()-1;
+        vector<pair<int,int>> v;
+        pair<int,int> p;
+        for(int i=0;i<nums.size();i++)
+        {
+            v.push_back({nums[i],i});
+        }
+        
+        sort(v.begin(),v.end());
+        
+        while(low<high)
+        {
+            if(v[low].first+v[high].first==target)
+            {
+                p.first=v[low].second;
+                p.second=v[high].second;
+                low++;
+                high--;
+            }
+            
+            else if(v[low].first+v[high].first<target)
+                low++;
+            else high--;
+        }
+        vector<int> vv;
+        vv.push_back(p.first);
+        vv.push_back(p.second);
+        return vv;
+       
+    }
+};
