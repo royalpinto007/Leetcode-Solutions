d0485a0d046ae1a6075c67a94400946c89183870
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-08-31 11:34:19 +0530

    Update 26-remove-duplicates-from-sorted-array.cpp

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -13,3 +13,13 @@ public:
         return j+1;
     }
 };
+
+/*
+int count = 0;
+for(int i = 1; i < n; i++)
+	{
+    		if(A[i] == A[i-1]) count++;
+    		else A[i-count] = A[i];
+	}
+return n-count;
+*/
