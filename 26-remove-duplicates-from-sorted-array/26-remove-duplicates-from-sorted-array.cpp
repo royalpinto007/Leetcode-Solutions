94ccc619e3701503770aac4dcc7c9b0217d940f1
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-08-31 11:13:12 +0530

    Update 26-remove-duplicates-from-sorted-array.cpp

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -1,9 +1,11 @@
-class Solution {
+class Solution 
+{
 public:
-     int removeDuplicates(vector<int>& nums) {
-        
-		int j=0;
-        for(int i=1;i<nums.size();i++){
+     int removeDuplicates(vector<int>& nums)
+     {
+        int j=0;
+        for(int i=1;i<nums.size();i++)
+	{
             if(nums[j]!=nums[i]) 
                 j++;
                 nums[j]=nums[i];  
