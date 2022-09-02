30f25bc14a0e62cf31116d8ec3ed472e4f93c745
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-09-02 07:37:47 +0530

    Update 50-powx-n.cpp

--- 50-powx-n/50-powx-n.cpp
+++ 50-powx-n/50-powx-n.cpp
@@ -12,3 +12,66 @@ public:
         return res;
     }
 };
+
+/*
+
+class Solution
+{
+public:
+    double pow(double x, int n)
+    {
+        if (n < 0)
+        {
+            x = 1.0 / x;
+        }
+        
+        double result = 1.0;
+        double power = x;
+        
+        while (n != 0)
+        {
+            if (n % 2 != 0)
+            {
+                result *= power;
+            }
+            n /= 2;
+            power = power * power;
+        }
+        
+        return result;
+    }
+};
+
+//======================================================================
+
+class Solution2
+{
+public:
+    double pow(double x, int n)
+    {
+        if (n == 0)
+        {
+            return 1.0;
+        }
+        
+        int h = n / 2;
+        int r = n - h * 2;
+        
+        */
+        double result = pow(x, h);
+        result *= result;
+        if (r == 1)
+        {
+            return result * x;
+        }
+        else if (r == 0)
+        {
+            return result;
+        }
+        else
+        {
+            return result / x;
+        }
+    }
+};
+*/
