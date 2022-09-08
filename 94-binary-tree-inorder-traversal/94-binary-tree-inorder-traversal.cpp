6d24dcac9e649b9861b66a0016598b6bfb6cb36c
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-09-08 05:48:33 +0530

    Time: 3 ms (60.90%), Space: 7.8 MB (99.77%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.cpp
@@ -11,21 +11,55 @@
  */
 class Solution {
 public:
-    vector<int> ans;
-    
-    void inorderTraversalR(TreeNode* root){
-        if(!root) return;
-        if(root->left){
-            inorderTraversal(root->left);
-        }
-        ans.push_back(root->val);
-        if(root->right){
-            inorderTraversal(root->right);
-        }
-    }
-    
     vector<int> inorderTraversal(TreeNode* root) {
-        inorderTraversalR(root);
+        stack<TreeNode*> stk;
+        TreeNode* cur = root;
+        vector<int> ans;
+        
+        stk.push(cur);
+        
+        do{
+            cur = stk.top();
+            
+            //go to its leftmost child
+            TreeNode* tmp = cur ? cur->left : NULL;
+            while(tmp){
+                stk.push(tmp);
+                tmp = tmp->left;
+            }
+            
+            //if it has left child, process leftmost child first
+            cur = stk.top(); stk.pop();
+            if(cur) ans.push_back(cur->val);
+            
+            //cut current node and its parent, 
+            //so current node won't be traversed again
+            if(stk.size() > 0) stk.top()->left = NULL;
+            
+            //deal with right child
+            if(cur && cur->right) stk.push(cur->right);
+        }while(!stk.empty());
+        
         return ans;
     }
 };
+// class Solution {
+// public:
+//     vector<int> ans;
+    
+//     void inorderTraversalR(TreeNode* root){
+//         if(!root) return;
+//         if(root->left){
+//             inorderTraversal(root->left);
+//         }
+//         ans.push_back(root->val);
+//         if(root->right){
+//             inorderTraversal(root->right);
+//         }
+//     }
+    
+//     vector<int> inorderTraversal(TreeNode* root) {
+//         inorderTraversalR(root);
+//         return ans;
+//     }
+// };
