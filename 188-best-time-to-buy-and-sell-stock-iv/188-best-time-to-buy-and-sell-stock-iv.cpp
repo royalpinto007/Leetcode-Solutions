9d7b958dd12cb48f3fee8e5c025b73bfb9fe6c87
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-09-10 06:31:26 +0530

    Time: 10 ms (82.16%), Space: 10.8 MB (84.96%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -2,10 +2,19 @@ class Solution {
 public:
     int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
+        int ans = 0;
+        
+        //we can do as many transactions as we want
+        if(k >= n/2){
+            for(int i = 1; i < n; i++){
+                ans += max(0, prices[i]-prices[i-1]);
+            }
+            return ans;
+        }
+        
         //0th element for padding
         vector<int> holds(min(n,k)+1, INT_MIN);
         vector<int> cashs(min(n,k)+1, 0);
-        int ans = 0;
         
         for(int price : prices){
             // cout << "price: " << price << endl;
@@ -30,6 +39,41 @@ public:
         return ans;
     }
 };
+
+// class Solution {
+// public:
+//     int maxProfit(int k, vector<int>& prices) {
+//         int n = prices.size();
+//         //0th element for padding
+//         vector<int> holds(min(n,k)+1, INT_MIN);
+//         vector<int> cashs(min(n,k)+1, 0);
+//         int ans = 0;
+        
+//         for(int price : prices){
+//             // cout << "price: " << price << endl;
+//             for(int i = min(n,k); i >= 1; i--){
+//                 /*
+//                 we should update cashs[i] and then holds[i]!
+//                 because when we update cashs[i],
+//                 we need holds[i] in last iteration!
+//                 */
+//                 /*
+//                 hold-cash makes a transaction,
+//                 so holds[i]+price means 
+//                 selling the stock bought in ith transaction
+//                 */
+//                 cashs[i] = max(cashs[i], holds[i]+price);
+//                 holds[i] = max(holds[i], cashs[i-1]-price);
+//                 // cout << i << ", " << holds[i] << ", " << cashs[i] << endl;
+//                 ans = max(ans, cashs[i]);
+//             }
+//         }
+        
+//         return ans;
+//     }
+// };
+
+
 // class Solution {
 // public:
 //     int maxProfit(int k, vector<int>& prices) {
