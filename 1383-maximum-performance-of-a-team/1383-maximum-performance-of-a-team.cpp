6780dd756b56ac8205ecbde47f01daf1b3f42246
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-09-11 09:20:59 +0530

    Time: 162 ms (35.39%), Space: 36.7 MB (78.87%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
@@ -1,38 +1,63 @@
+// Time:  O(nlogn)
+// Space: O(n)
 class Solution {
 public:
-    int maxPerformance(int n, vector<int>& speed, vector<int>& efficiency, int k) 
-    {
-        vector<pair<int, int>> ess;
-        for (int i = 0; i < n; i++)
-        {
-            ess.push_back({efficiency[i], speed[i]});
+    int maxPerformance(int n, vector<int>& speed, vector<int>& efficiency, int k) {
+        static const int MOD = 1e9 + 7;
+        uint64_t result = 0, s_sum = 0;
+        vector<pair<int, int>> engineers;
+        for (int i = 0; i < speed.size(); ++i) {
+            engineers.emplace_back(efficiency[i], speed[i]);
         }
-        //sort by efficiency and then by speed, descending
-        sort(ess.rbegin(), ess.rend());
-
-        long sumS = 0, res = 0;
-        priority_queue <int, vector<int>, greater<int> > pq;
-        //itereate through efficiency
-        for (int i = 0; i < n; i++) 
-        {
-            //for each efficiency, find the maximum possible combination(of size <= k) of speed
-            //possible speeds are the speed whose efficiency >= current efficiency
-            pq.push(ess[i].second);
-            //sumS: the sum of the combination of speed for current efficiency
-            sumS += ess[i].second;
-            // cout << "push " << ess[i].second << ", sumS: " << sumS << endl;
-            if (pq.size() > k) 
-            {
-                sumS -= pq.top();
-                // cout << "pop " << pq.top() << ", sumS: " << sumS << endl;
-                pq.pop();
+        sort(engineers.begin(), engineers.end(), greater<pair<int, int>>());
+        priority_queue<int, vector<int>, greater<int>> min_heap;
+        for (const auto& [e, s] : engineers) {
+            s_sum += s;
+            min_heap.emplace(s);
+            if (min_heap.size() > k) {
+                s_sum -= min_heap.top(); min_heap.pop();
             }
-            // cout << "current res: " << sumS * ess[i].first << endl;
-            res = max(res, sumS * ess[i].first);
+            result = max(result, s_sum * e);
         }
-        return res % (int)(1e9+7);
+        return result % MOD;
     }
 };
+
+// class Solution {
+// public:
+//     int maxPerformance(int n, vector<int>& speed, vector<int>& efficiency, int k) 
+//     {
+//         vector<pair<int, int>> ess;
+//         for (int i = 0; i < n; i++)
+//         {
+//             ess.push_back({efficiency[i], speed[i]});
+//         }
+//         //sort by efficiency and then by speed, descending
+//         sort(ess.rbegin(), ess.rend());
+
+//         long sumS = 0, res = 0;
+//         priority_queue <int, vector<int>, greater<int> > pq;
+//         //itereate through efficiency
+//         for (int i = 0; i < n; i++) 
+//         {
+//             //for each efficiency, find the maximum possible combination(of size <= k) of speed
+//             //possible speeds are the speed whose efficiency >= current efficiency
+//             pq.push(ess[i].second);
+//             //sumS: the sum of the combination of speed for current efficiency
+//             sumS += ess[i].second;
+//             // cout << "push " << ess[i].second << ", sumS: " << sumS << endl;
+//             if (pq.size() > k) 
+//             {
+//                 sumS -= pq.top();
+//                 // cout << "pop " << pq.top() << ", sumS: " << sumS << endl;
+//                 pq.pop();
+//             }
+//             // cout << "current res: " << sumS * ess[i].first << endl;
+//             res = max(res, sumS * ess[i].first);
+//         }
+//         return res % (int)(1e9+7);
+//     }
+// };
 
 // TLE
 // class Solution {
