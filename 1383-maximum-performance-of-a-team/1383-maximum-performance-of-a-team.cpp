a0a427474d08fb0656dbd2eaa834fcdffffd7219
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-09-11 09:19:20 +0530

    Time: 242 ms (13.56%), Space: 36.8 MB (56.16%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
@@ -1,8 +1,10 @@
 class Solution {
 public:
-    int maxPerformance(int n, vector<int>& speed, vector<int>& efficiency, int k) {
+    int maxPerformance(int n, vector<int>& speed, vector<int>& efficiency, int k) 
+    {
         vector<pair<int, int>> ess;
-        for (int i = 0; i < n; i++){
+        for (int i = 0; i < n; i++)
+        {
             ess.push_back({efficiency[i], speed[i]});
         }
         //sort by efficiency and then by speed, descending
@@ -11,14 +13,16 @@ public:
         long sumS = 0, res = 0;
         priority_queue <int, vector<int>, greater<int> > pq;
         //itereate through efficiency
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) 
+        {
             //for each efficiency, find the maximum possible combination(of size <= k) of speed
             //possible speeds are the speed whose efficiency >= current efficiency
             pq.push(ess[i].second);
             //sumS: the sum of the combination of speed for current efficiency
             sumS += ess[i].second;
             // cout << "push " << ess[i].second << ", sumS: " << sumS << endl;
-            if (pq.size() > k) {
+            if (pq.size() > k) 
+            {
                 sumS -= pq.top();
                 // cout << "pop " << pq.top() << ", sumS: " << sumS << endl;
                 pq.pop();
@@ -30,7 +34,41 @@ public:
     }
 };
 
+// TLE
+// class Solution {
+// public:
+//     int maxPerformance(int n, vector<int>& speed, vector<int>& efficiency, int k) {
+//         vector<pair<int, int>> ess;
+//         for(int i = 0; i < n; i++){
+//             ess.emplace_back(efficiency[i], speed[i]);
+//         }
+//         //descending
+//         sort(ess.rbegin(), ess.rend());
+        
+//         //sorted array of speeds, descending
+//         vector<int> speedComb;
+        
+//         long sSum = 0, res = 0;
+        
+//         for(int i = 0; i < n; i++){
+//             //use insertion sort
+//             vector<int>::iterator it = find_if(speedComb.begin(), speedComb.end(), 
+//                 [&ess, &i](int e){return e < ess[i].second;});
+//             speedComb.insert(it, ess[i].second);
+//             sSum += ess[i].second;
+//             if(speedComb.size() > k){
+//                 sSum -= speedComb[speedComb.size()-1];
+//                 speedComb.pop_back();
+//             }
+//             res = max(res, sSum * ess[i].first);
+//         }
+        
+//         return res % (int)(1e9+7);
+//     }
+// };
 
+
+// TLE
 // class Solution {
 // public:
 //     int ans;
