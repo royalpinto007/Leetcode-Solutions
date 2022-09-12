5acc94ba3a9ea852e9ce6e9af9293eae683ef662
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-09-12 17:44:08 +0530

    Time: 16 ms (18.97%), Space: 10.4 MB (94.44%) - LeetHub

--- 948-bag-of-tokens/948-bag-of-tokens.cpp
+++ 948-bag-of-tokens/948-bag-of-tokens.cpp
@@ -1,17 +1,26 @@
-class Solution {
+class Solution 
+{
 public:
-    int bagOfTokensScore(vector<int>& tokens, int P) {
+    int bagOfTokensScore(vector<int>& tokens, int P) 
+    {
         sort(tokens.begin(), tokens.end());
         int result = 0, points = 0;
         int left = 0, right = tokens.size() - 1;
-        while (left <= right) {
-            if (P >= tokens[left]) {
+        
+        while (left <= right) 
+        {
+            if (P >= tokens[left]) 
+            {
                 P -= tokens[left++];
                 result = max(result, ++points);
-            } else if (points > 0) {
+            } 
+            else if (points > 0) 
+            {
                 --points;
                 P += tokens[right--];
-            } else {
+            } 
+            else 
+            {
                 break;
             }
         }
