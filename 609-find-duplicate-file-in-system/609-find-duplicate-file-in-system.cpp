6125bd14a4dd1191ac88a8ff6be06a4190005e0b
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-09-19 08:43:05 +0530

    Time: 208 ms (37.02%), Space: 38.4 MB (58.78%) - LeetHub

---
+++ 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<string>> findDuplicate(vector<string>& paths) {
+        unordered_map<string, vector<string>> files;
+        for (const auto& path : paths) {
+    	    stringstream ss(path);
+    	    string root;
+    	    string s;
+    	    getline(ss, root, ' ');
+    	    while (getline(ss, s, ' ')) {
+    		    auto fileName = root + '/' + s.substr(0, s.find('('));
+    		    auto fileContent = s.substr(s.find('(') + 1, s.find(')') - s.find('(') - 1);
+    		    files[fileContent].emplace_back(fileName);
+    	    }
+        }
+    
+        vector<vector<string>> result;
+        for (const auto& file : files) {
+    	    if (file.second.size() > 1) {
+    		    result.emplace_back(file.second);
+    	    }
+        }
+        return result;
+    }
+};
