5e87a3b42067fe4f1bcf0e8dce142b62c9af27a5
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-09-20 08:29:15 +0530

    Time: 383 ms (69.86%), Space: 109.2 MB (14.62%) - LeetHub

---
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int findLength(vector<int>& A, vector<int>& B) {
+        vector<vector<int>>dp(A.size()+1, vector<int>(B.size()+1,0));
+        int res = 0;
+        for(int i = 0; i<A.size(); i++)
+            for(int j = 0; j<B.size(); j++)
+                if(A[i] == B[j]){
+                    dp[i+1][j+1] = dp[i][j]+1; //dp[i+1][j+1] 
+                    res = max(res,dp[i+1][j+1]);
+                }
+        return res;
+    }
+};
+
+
+// class Solution {
+// public:
+//     int findLength(vector<int>& A, vector<int>& B) {
+//         vector<vector<int>>dp(A.size()+1, vector<int>(B.size()+1,0));
+//         int res = 0;
+//         for(int i = A.size()-1; i>=0; i--)
+//             for(int j = B.size() - 1; j>=0; j--)
+//                 if(A[i] == B[j]){
+//                     dp[i][j] = dp[i+1][j+1]+1;
+//                     res = max(res,dp[i][j]);
+//                 }
+//         return res;
+//     }
+// };
