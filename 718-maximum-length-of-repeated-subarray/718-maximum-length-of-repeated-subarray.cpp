80d264a1d0887350ddd77e9449951b5ce3c155ab
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-09-20 08:30:46 +0530

    Time: 470 ms (53.82%), Space: 11.9 MB (95.52%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -1,10 +1,10 @@
 class Solution {
 public:
     int findLength(vector<int>& A, vector<int>& B) {
-        vector<int>dp(B.size()+1,0); //loop through B 每一个Loop through A的，现在A跟上一个A比较
+        vector<int>dp(B.size()+1,0); 
         int res = 0;
         for(int i = 0; i<A.size(); i++)
-            for(int j = B.size()-1; j>=0; j--) // 从后往前的原因是，避免从0到B。size() 假如用到dp[5] = dp[4] + 1, 而上一个循环因为不match，把dp[4] 设置成0了
+            for(int j = B.size()-1; j>=0; j--)
                 res = max(res, dp[j+1] = A[i] == B[j] ? dp[j]+1: 0);
                 
         return res;
