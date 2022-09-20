55516d3d58d192f365de5bb0e8e479b8e5356d1c
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-09-20 08:30:32 +0530

    Time: 258 ms (88.93%), Space: 11.8 MB (97.92%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -1,17 +1,30 @@
 class Solution {
 public:
     int findLength(vector<int>& A, vector<int>& B) {
-        vector<vector<int>>dp(A.size()+1, vector<int>(B.size()+1,0));
+        vector<int>dp(B.size()+1,0); //loop through B 每一个Loop through A的，现在A跟上一个A比较
         int res = 0;
         for(int i = 0; i<A.size(); i++)
-            for(int j = 0; j<B.size(); j++)
-                if(A[i] == B[j]){
-                    dp[i+1][j+1] = dp[i][j]+1; //dp[i+1][j+1] 
-                    res = max(res,dp[i+1][j+1]);
-                }
+            for(int j = B.size()-1; j>=0; j--) // 从后往前的原因是，避免从0到B。size() 假如用到dp[5] = dp[4] + 1, 而上一个循环因为不match，把dp[4] 设置成0了
+                res = max(res, dp[j+1] = A[i] == B[j] ? dp[j]+1: 0);
+                
         return res;
     }
 };
+
+// class Solution {
+// public:
+//     int findLength(vector<int>& A, vector<int>& B) {
+//         vector<vector<int>>dp(A.size()+1, vector<int>(B.size()+1,0));
+//         int res = 0;
+//         for(int i = 0; i<A.size(); i++)
+//             for(int j = 0; j<B.size(); j++)
+//                 if(A[i] == B[j]){
+//                     dp[i+1][j+1] = dp[i][j]+1; //dp[i+1][j+1] 
+//                     res = max(res,dp[i+1][j+1]);
+//                 }
+//         return res;
+//     }
+// };
 
 
 // class Solution {
