457d8eb9b17ce3e6cdaff095cf5fc527d237ed56
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-09-28 07:21:05 +0530

    Time: 11 ms (27.59%), Space: 10.7 MB (32.26%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -37,3 +37,106 @@ public:
         return head;
     }
 };
+
+
+/*
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* cur = head;
+        int len = 0;
+        
+        while(cur){
+            ++len;
+            cur = cur->next;
+        }
+        
+        if(n == len){
+            //delete the head
+            ListNode* ans = head->next;
+            delete head;
+            return ans;
+        }
+        
+        cur = head;
+        for(int i = 0; i < len; ++i){
+            if(i == len-n-1){
+                //the node previous to the node to be deleted
+                ListNode* to_delete = cur->next;
+                cur->next = cur->next->next;
+                delete to_delete;
+                break;
+            }
+            cur = cur->next;
+        }
+        
+        return head;
+    }
+};
+*/
+
+/*
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* dummy = new ListNode();
+        dummy->next = head;
+        
+        ListNode* cur;
+        int len = 0;
+        
+        cur = dummy->next;
+        while(cur){
+            ++len;
+            cur = cur->next;
+        }
+        
+        // cout << "len: " << len << endl;
+        
+        cur = dummy;
+        for(int i = 0; i < len; ++i, cur = cur->next){
+            //i is 1-based
+            // cout << i << " : " << cur->val << endl;
+            if(i == len-n){
+                //this is the node previous to the node to be deleted
+                ListNode* tmp = cur->next;
+                cur->next = cur->next->next;
+                delete tmp;
+                break;
+            }
+        }
+        
+        return dummy->next;
+    }
+};
+*/
+
+/*
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* dummy = new ListNode();
+        dummy->next = head;
+        
+        ListNode *slow = dummy, *fast = dummy;
+        
+        for(int i = 0; i < n+1; ++i){
+            fast = fast->next;
+        }
+        //slow and fast are n+1 apart
+        
+        while(fast){
+            slow = slow->next;
+            fast = fast->next;
+        }
+        //slow is n+1 from tail->next, so n from tail
+        //so it's (n+1)-last node
+        
+        ListNode* tmp = slow->next;
+        slow->next = slow->next->next;
+        delete tmp;
+        
+        return dummy->next;
+    }
+};
+*/
