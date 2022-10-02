7ec800832e90e5c251888a148edf45def3d6c6e1
Royal Simpson Pinto <97520428+royalpinto007@users.noreply.github.com>
2022-10-02 08:21:35 +0530

    Time: 221 ms (5.45%), Space: 8.6 MB (21.99%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -15,3 +15,27 @@ public:
         return dp[d % 2][target] % MOD;
     }
 };
+
+/*
+class Solution {
+public:
+    const int MOD = 1e9+7;
+    int numRollsToTarget(int d, int f, int target) 
+    {
+        vector<vector<int>> dp(d+1,vector<int>(target+1,0));
+        dp[0][0] = 1; // Because with dice 1 and we can make sum 1 in 1 way
+        for(int dice=1;dice<=d;dice++)
+        {
+            for(int t=1;t<=target;t++)
+            {
+                for(int face=1;face<=f;face++)
+                {
+                    if(t>=face)
+                        dp[dice][t] = (dp[dice][t]%MOD + dp[dice-1][t-face]%MOD)%MOD;
+                }
+            }
+        }
+        return dp[d][target];
+    }
+};
+*/
