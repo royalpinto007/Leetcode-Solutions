1215e6a7bb70f9d942af10a91e93b018b08ec61c
Royal Simpson Pinto <royalpinto007@gmail.com>
2022-10-07 06:39:45 +0530

    Time: 268 ms (28.12%), Space: 26.4 MB (37.06%) - LeetHub

--- 732-my-calendar-iii/732-my-calendar-iii.cpp
+++ 732-my-calendar-iii/732-my-calendar-iii.cpp
@@ -22,3 +22,54 @@ public:
  * MyCalendarThree* obj = new MyCalendarThree();
  * int param_1 = obj->book(start,end);
  */
+
+/*
+class MyCalendarThree {
+private:
+    unordered_map<int, int> vals;
+    unordered_map<int, int> lazy;
+
+public:
+    MyCalendarThree() {}
+
+    void update(int start, int end, int left, int right, int idx) {
+        if (start > right || end < left) return;
+        if (left >= start && right <= end) {
+            lazy[idx]++;
+            vals[idx]++;
+        } else {
+            int mid = (left + right) / 2;
+            update(start, end, left, mid, idx * 2);
+            update(start, end, mid + 1, right, idx * 2 + 1);
+            vals[idx] = lazy[idx] + max(vals[idx * 2], vals[idx * 2 + 1]);
+        }
+    }
+
+    int book(int start, int end) {
+        update(start, end - 1, 1, 1000000000, 1);
+        return vals[1];
+    }
+};
+*/
+
+/*
+class MyCalendarThree {
+private:
+    map<int, int> starts;
+    int res;
+
+public:
+    MyCalendarThree() {
+        starts[0] = 0, starts[1e9 + 1] = 0;
+        res = 0;
+    }
+    void split(int x) { starts[x] = (--starts.upper_bound(x))->second; }
+    int book(int start, int end) {
+        split(start), split(end);
+        for (auto it = starts.find(start); it->first < end; it++) {
+            res = max(res, ++(it->second));
+        }
+        return res;
+    }
+};
+*/
