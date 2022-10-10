c93ef4dee7dcc34e3c34ffd762b8df090b72da19
Royal Simpson Pinto <royalpinto007@gmail.com>
2022-10-10 06:51:58 +0530

    Time: 4 ms (29.56%), Space: 6.2 MB (45.53%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string breakPalindrome(string s) { // O(N)
+        int n = s.length();
+        if(n == 1) {
+            return "";
+        }
+        for(int i = 0; i < n; i++) {
+            int j = n - 1 - i;
+            if(i == j) {
+                continue;
+            }
+            if(s[i] != 'a') {
+                s[i] = 'a';
+                return s;
+            }
+        }
+        s[n-1] = 'b';
+        return s;
+    }
+};
