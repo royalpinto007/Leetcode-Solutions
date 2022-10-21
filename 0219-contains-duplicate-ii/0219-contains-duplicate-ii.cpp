35a4ec72cdd1747151eda83ec3ae11394260f3f9
Royal Simpson Pinto <royalpinto007@gmail.com>
2022-10-21 06:58:41 +0530

    Time: 274 ms (72.92%), Space: 80.4 MB (15.96%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,19 +1,36 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        for(int i = 0; i+1 < nums.size(); i++){
-            // cout << "[" << i+1 << ", " << i+k << "]" << endl;
-            int finish = min((int)nums.size(), i+k+1);
-            if(find(nums.begin()+i+1, nums.begin()+finish, nums[i])
-              != nums.begin()+finish){
-                // cout << endl;
-                return true;
-            }
+        set<int> myset;
+        for(int i = 0; i < nums.size(); i++){
+            //window : [i-k, i]
+            //so (i-k-1)th goes out of the window, need to remove it
+            if(i-k-1 >= 0) myset.erase(nums[i-k-1]);
+            /*
+            The single element versions (1) return a pair, with its member pair::first set to an iterator pointing to either the newly inserted element or to the equivalent element already in the set. The pair::second element in the pair is set to true if a new element was inserted or false if an equivalent element already existed.
+            */
+            if(!myset.insert(nums[i]).second) return true;
         }
-        // cout << endl;
         return false;
     }
 };
+
+// class Solution {
+// public:
+//     bool containsNearbyDuplicate(vector<int>& nums, int k) {
+//         for(int i = 0; i+1 < nums.size(); i++){
+//             // cout << "[" << i+1 << ", " << i+k << "]" << endl;
+//             int finish = min((int)nums.size(), i+k+1);
+//             if(find(nums.begin()+i+1, nums.begin()+finish, nums[i])
+//               != nums.begin()+finish){
+//                 // cout << endl;
+//                 return true;
+//             }
+//         }
+//         // cout << endl;
+//         return false;
+//     }
+// };
 
 // class Solution {
 // public:
