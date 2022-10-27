5ed51f75d8101649eeba3062167a86edc8feafdf
Royal Simpson Pinto <royalpinto007@gmail.com>
2022-10-27 07:57:02 +0530

    Time: 143 ms (62.50%), Space: 9.2 MB (100.00%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.cpp
+++ 0835-image-overlap/0835-image-overlap.cpp
@@ -20,3 +20,89 @@ public:
         return max_overlap;
     }
 };
+
+// class Solution {
+// public:
+//     int n;
+    
+//     int convolute(vector<vector<int>>& A, vector<vector<int>>& kernel, 
+//         int rshift, int cshift){
+//         int res = 0;
+        
+//         for(int r = 0; r < n; ++r){
+//             for(int c = 0; c < n; ++c){
+//                 res += A[r][c] * kernel[r+rshift][c+cshift];
+//             }
+//         }
+        
+//         return res;
+//     };
+    
+//     int largestOverlap(vector<vector<int>>& A, vector<vector<int>>& B) {
+//         n = A.size();
+        
+//         //padding every side for n-1
+//         //the original B is now at the center of B_padded
+//         vector<vector<int>> B_padded(3*n-2, vector<int>(3*n-2, 0));
+        
+//         for(int r = 0; r < n; ++r){
+//             for(int c = 0; c < n; ++c){
+//                 B_padded[r+n-1][c+n-1] = B[r][c];
+//             }
+//         }
+        
+//         int maxOverlaps = 0;
+        
+//         for(int rshift = 0; rshift+n-1 < 3*n-2; ++rshift){
+//             for(int cshift = 0; cshift+n-1 < 3*n-2; ++cshift){
+//                 maxOverlaps = max(maxOverlaps,
+//                     convolute(A, B_padded, rshift, cshift));
+//             }
+//         }
+        
+//         return maxOverlaps;
+//     }
+// };
+
+// class Solution {
+// public:
+//     struct pair_hash_int {
+//         inline std::size_t operator()(const std::pair<int,int> & v) const {
+//             return v.first*31+v.second;
+//         }
+//     };
+    
+//     void non_zero_cells(vector<vector<int>>& mat,
+//                        vector<pair<int, int>>& ones){
+//         int n = mat.size();
+        
+//         for(int i = 0; i < n; ++i){
+//             for(int j = 0; j < n; ++j){
+//                 if(mat[i][j]){
+//                     ones.push_back({i, j});
+//                 }
+//             }
+//         }
+//     };
+    
+//     int largestOverlap(vector<vector<int>>& A, vector<vector<int>>& B) {
+//         vector<pair<int, int>> A_ones;
+//         vector<pair<int, int>> B_ones;
+        
+//         non_zero_cells(A, A_ones);
+//         non_zero_cells(B, B_ones);
+        
+//         int maxOverlaps = 0;
+//         unordered_map<pair<int, int>, int, pair_hash_int> groupCount;
+        
+//         for(pair<int, int>& a : A_ones){
+//             for(pair<int, int>& b : B_ones){
+//                 pair<int, int> v = {b.first-a.first, b.second-a.second};
+//                 ++groupCount[v];
+//                 maxOverlaps = max(maxOverlaps, groupCount[v]);
+//             }
+//         }
+        
+//         return maxOverlaps;
+//     }
+// };
