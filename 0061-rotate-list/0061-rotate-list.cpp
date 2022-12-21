b7b428f22271595ca1329dee67167f34d7d7f52f
Royal Simpson Pinto <royalpinto007@gmail.com>
2022-12-21 12:49:01 +0530

    Time: 24 ms (6.00%), Space: 11.7 MB (89.38%) - LeetHub

---
+++ 0061-rotate-list/0061-rotate-list.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+// same problem as in Coding Ninjas (kReverse)
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+
+        ListNode* cur=head;
+        int length=0;
+        
+        if(k<=0|| head==NULL||head->next==NULL)
+            return head;
+
+        // Find length of the linked list
+        while(cur){
+            cur=cur->next;
+            length++;
+        }
+        // set the number of rotations
+        k=k%length;
+
+        //Return head if no rotation is required
+        if(k==0){
+            return head;
+        }
+
+        //set the current node as head and iterate till the node  after which we would need to bring it forward.
+        cur=head;
+        int n=length-k;
+        while(--n){
+            cur=cur->next;
+        }
+
+        //set temp as the node next to the current node
+        ListNode* temp=cur->next;
+
+        //set the next of current node as NULL 
+        cur->next=NULL;
+
+        //set head1 as temp(from which we need to bring forward)
+        ListNode* head1=temp;
+
+        //iterate to the end
+        while(temp->next){
+            temp=temp->next;
+        }
+        //set the next of the last node as head.
+        temp->next=head;
+        
+        return head1;
+
+    } 
+};
