be14d7a4045a361b723c3bedda896cd4cffcec5c
Royal Simpson Pinto <royalpinto007@gmail.com>
2022-12-21 11:55:14 +0530

    Time: 43 ms (82.23%), Space: 71.4 MB (82.56%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -18,6 +18,7 @@ public:
         ListNode* temp=dummy;
         int carry=0;
         
+        // to perform- sum = l1 + l2 + carry
         while(l1!=NULL || l2!=NULL || carry){
             int sum=0;
             
@@ -32,10 +33,10 @@ public:
             
             sum+=carry;
             carry=sum/10;
-            ListNode* newnode=new ListNode(sum%10);
-            temp->next=newnode;
-            temp=temp->next;
+            ListNode* newnode=new ListNode(sum%10); // the value we'll get by moduloing it, will become as new node so. add it to our list 
+            temp->next=newnode; // temp will point to that new node if we get
+            temp=temp->next; // update the temp every time
         }
-        return dummy->next;
+        return dummy->next; // return dummy->next because, we don't want the value that we have consider in it intially!!
     }
 };
