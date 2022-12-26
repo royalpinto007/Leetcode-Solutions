fedd0b6f829d4a974c02e01154e6fbcba307313c
Royal Simpson Pinto <royalpinto007@gmail.com>
2022-12-26 17:36:42 +0530

    Time: 61 ms (96.57%), Space: 48.4 MB (48.80%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        
+        int n=nums.size(), canReach=0;
+        
+        for(int i=0;i<n;i++){
+            if(canReach<i) return 0;
+            canReach=max(canReach,i+nums[i]);
+        }
+        return 1;
+    }
+};
