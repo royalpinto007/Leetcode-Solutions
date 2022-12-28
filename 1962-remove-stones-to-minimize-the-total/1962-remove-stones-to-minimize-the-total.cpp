3ab2566b1a1e368b56b0dbfd440cd67cc9d185d2
Royal Simpson Pinto <royalpinto007@gmail.com>
2022-12-28 11:09:59 +0530

    Time: 565 ms (89.31%), Space: 98.9 MB (84.14%) - LeetHub

---
+++ 1962-remove-stones-to-minimize-the-total/1962-remove-stones-to-minimize-the-total.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minStoneSum(vector<int>& piles, int k) {
+        
+        priority_queue<int> heap(piles.begin(), piles.end());
+        
+        // accumulate(first, last, sum);
+        int totalSum = accumulate(piles.begin(), piles.end(), 0); 
+        
+        for (int i = 0; i < k; i++) {
+            int curr = heap.top();
+            heap.pop();
+            int remove = curr / 2;
+            totalSum -= remove;
+            heap.push(curr - remove);
+        }
+        
+        return totalSum;
+    }
+};
